f7b65c59024b8dbb9b8be10c59eaebde298f098c
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-14 23:05:27 +0200

    monsters-rolodex: Add console.log for props and its children

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,5 +1,7 @@
 import { Component } from 'react'
 
+import { CardList } from './components/card-list/card-list.component'
+
 class App extends Component {
   constructor() {
     super()
@@ -18,6 +20,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <CardList name='Yihua'><h1>Yihua</h1></CardList>
         {this.state.monsters.map((monster) => (
           <h1 key={monster.id}>{monster.name}</h1>
         ))}
