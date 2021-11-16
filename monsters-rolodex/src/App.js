4bc07d80453d0c04945ef416556124516bdbf262
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-16 04:58:36 +0200

    monsters-rolodex: Add some syntax changes

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -30,6 +30,7 @@ class App extends Component {
     const filteredMonsters = monsters.filter((monster) =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
     )
+
     return (
       <div className="App">
         <h1>Monsters Rolodex</h1>
--- monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -2,12 +2,10 @@ import { Card } from '../card/card.component'
 
 import './card-list.styles.css'
 
-export const CardList = (props) => {
-  return (
-    <div className="card-list">
-      {props.monsters.map((monster) => (
-        <Card key={monster.id} monster={monster} />
-      ))}
-    </div>
-  )
-}
+export const CardList = (props) => (
+  <div className="card-list">
+    {props.monsters.map((monster) => (
+      <Card key={monster.id} monster={monster} />
+    ))}
+  </div>
+)
