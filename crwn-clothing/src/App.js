ace7193c7760914d64c54195517c7122a8747f3d
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-18 18:15:26 +0200

    crwn-clothing: Add shop component

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom'
 
 // components
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 
 // styles
 import './App.sass'
@@ -12,6 +13,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   )
---
+++ crwn-clothing/src/pages/shop/shop.component.jsx
@@ -0,0 +1,21 @@
+// modules
+import { Component } from 'react'
+
+// components
+import SHOP_DATA from './shop.data'
+
+class ShopPage extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      collections: SHOP_DATA,
+    }
+  }
+
+  render() {
+    return <div>SHOP PAGE</div>
+  }
+}
+
+export default ShopPage
