19792264dd0049aba70890059f9908b790d54238
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-19 03:07:36 +0200

    crwn-clothing: Add sign in component

---
+++ crwn-clothing/src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,65 @@
+// modules
+import { Component } from 'react'
+
+// components
+import FormInput from '../form-input/form-input.component'
+import CustomButton from '../custom-button/custom-button.component'
+
+// styles
+import './sign-in.styles.sass'
+
+class SignIn extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      email: '',
+      password: '',
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+
+    this.setState({ email: '', password: '' })
+  }
+
+  handleChange = (event) => {
+    const { value, name } = event.target
+    this.setState({ [name]: value })
+  }
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            type="email"
+            name="email"
+            id="email"
+            value={this.state.email}
+            handleChange={this.handleChange}
+            label="email"
+            required
+          />
+          <FormInput
+            type="password"
+            name="password"
+            id="password"
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label="password"
+            required
+          />
+
+          <CustomButton type="submit">Sign In</CustomButton>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default SignIn
