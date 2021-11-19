656896c0bb72370f9fa2ceec06419a9fd13783f5
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-19 03:07:50 +0200

    crwn-clothing: Add form-input component

---
+++ crwn-clothing/src/components/form-input/form-input.component.jsx
@@ -0,0 +1,19 @@
+// styles
+import './form-input.styles.sass'
+
+const FormInput = ({ handleChange, label, ...otherProps }) => (
+  <div className="group">
+    <input className="form-input" onChange={handleChange} {...otherProps} />
+    {label ? (
+      <label
+        className={`${
+          otherProps.value.length ? 'shrink' : ''
+        } form-input-label`}
+      >
+        {label}
+      </label>
+    ) : null}
+  </div>
+)
+
+export default FormInput
