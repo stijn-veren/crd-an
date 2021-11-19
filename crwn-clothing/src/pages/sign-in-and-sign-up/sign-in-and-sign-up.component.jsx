a39f51c4b831bc569d6decf522cac028776251aa
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-19 03:07:25 +0200

    crwn-clothing: Add sign in and sing up component

---
+++ crwn-clothing/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,13 @@
+// components
+import SignIn from '../../components/sign-in/sign-in.component'
+
+// styles
+import './sign-in-and-sign-up.styles.sass'
+
+const SignInAndSignUpPage = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
+)
+
+export default SignInAndSignUpPage
