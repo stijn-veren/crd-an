3b19cb0b9185b0bf0c58177ed7e0332a2bc27acb
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-21 01:49:28 +0200

    crwn-clothing: Add firebase auth and show user object in console

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,4 +1,5 @@
 // modules
+import { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 // components
@@ -6,21 +7,39 @@ import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import { auth } from './firebase/firebase.utils'
 
 // styles
 import './App.sass'
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  )
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      currentUser: null,
+    }
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user })
+      console.log(user)
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App
