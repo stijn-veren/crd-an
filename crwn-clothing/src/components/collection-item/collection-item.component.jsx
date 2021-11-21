8a3621603325c8658fb07a298515215bc672f932
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-21 04:06:33 +0200

    crwn-clothing: Minor syntax changes

--- crwn-clothing/src/components/collection-item/collection-item.component.jsx
+++ crwn-clothing/src/components/collection-item/collection-item.component.jsx
@@ -3,7 +3,12 @@ import './collection-item.styles.sass'
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
   <div className="collection-item">
-    <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+    <div
+      className="image"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
     <div className="collection-footer">
       <span className="name">{name}</span>
       <span className="price">{price}</span>
