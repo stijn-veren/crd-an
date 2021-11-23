2594162311b73a51e69e44009e3aa999d84b40b4
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-24 00:05:18 +0200

    crwn-clothing: Add a possibility to remove items from cart

--- crwn-clothing/src/components/checkout-item/checkout-item.component.jsx
+++ crwn-clothing/src/components/checkout-item/checkout-item.component.jsx
@@ -1,16 +1,32 @@
+// modules
+import { connect } from 'react-redux'
+
+// components
+import { clearItemFromCart } from '../../redux/cart/cart.actions'
+
 // styles
 import './checkout-item.styles.sass'
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => (
-  <div className="checkout-item">
-    <div className="image-container">
-      <img src={imageUrl} alt="item" />
-    </div>
-    <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">{price}</span>
-    <div className="remove-button">&#10005;</div>
-  </div>
-)
+const CheckoutItem = ({ cartItem, clearItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem
 
-export default CheckoutItem
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt="item" />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">{quantity}</span>
+      <span className="price">{price}</span>
+      <div className="remove-button" onClick={() => clearItem(cartItem)}>
+        &#10005;
+      </div>
+    </div>
+  )
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  clearItem: (item) => dispatch(clearItemFromCart(item)),
+})
+
+export default connect(null, mapDispatchToProps)(CheckoutItem)
