6d9b32cd7967a5f7db7a1c897b24c2cc9b5b2b68
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-23 23:43:37 +0200

    crwn-clothing: Add dispatch action shorthand

--- crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -7,11 +7,12 @@ import { withRouter } from 'react-router'
 import CustomButton from '../custom-button/custom-button.component'
 import CartItem from '../cart-item/cart-item.component'
 import { selectCartItems } from '../../redux/cart/cart.selectors'
+import { toggleCartHidden } from '../../redux/cart/cart.actions'
 
 // styles
 import './cart-dropdown.styles.sass'
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
       {cartItems.length ? (
@@ -22,7 +23,12 @@ const CartDropdown = ({ cartItems, history }) => (
         <span className="empty-message">Your cart is empty</span>
       )}
     </div>
-    <CustomButton onClick={() => history.push('./checkout')}>
+    <CustomButton
+      onClick={() => {
+        history.push('./checkout')
+        dispatch(toggleCartHidden())
+      }}
+    >
       GO TO CHECKOUT
     </CustomButton>
   </div>
