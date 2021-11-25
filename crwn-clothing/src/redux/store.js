7ca751a0c11286b56e9b1c2d15daa76be0311d85
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-25 04:37:31 +0200

    crwn-clothing: Add using middlewares by condition

--- crwn-clothing/src/redux/store.js
+++ crwn-clothing/src/redux/store.js
@@ -6,7 +6,11 @@ import logger from 'redux-logger'
 // components
 import rootReducer from './root-reducer'
 
-const middlewares = [logger]
+const middlewares = []
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger)
+}
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
