395f10193442a590e0948624e766fa3fe1f95095
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-25 04:56:44 +0200

    crwn-clothing: Use styled components in homepage

--- crwn-clothing/src/pages/homepage/homepage.component.jsx
+++ crwn-clothing/src/pages/homepage/homepage.component.jsx
@@ -2,12 +2,12 @@
 import Directory from '../../components/directory/directory.component'
 
 // styles
-import './homepage.styles.sass'
+import { HomePageContainer } from './homepage.styles'
 
 const HomePage = () => (
-  <div className="homepage">
+  <HomePageContainer>
     <Directory />
-  </div>
+  </HomePageContainer>
 )
 
 export default HomePage
